ad04de4cdcf7142ae1c0140c8d78198311d6c87c
JulesBarbe <ajules32@gmail.com>
2021-02-23 22:33:50 -0500

    Structural changes

---
+++ Scripts/Comparator.py
@@ -0,0 +1,7 @@
+#Use TFIDF vectorized matrix and run LSA analysis on it
+
+#Use results to seach articles from trusted sites
+
+#Take found articles, extract text and run LSA analysis on them as well
+
+#Compute difference as percentage
---
+++ Scripts/Preprocessing.py
@@ -0,0 +1,4 @@
+#Given a text, run different preprocessing techniques
+#Looking meanly at tfidf and stopword removal for now, can add more later
+#Ideally we could make a function with parameters so that we could run tfidf, but without stop word removal too
+#(would be good for the different title implementation)
