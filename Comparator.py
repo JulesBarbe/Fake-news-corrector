b84d59fae03e4ff4867b9b5c96052f5c52638546
JulesBarbe <ajules32@gmail.com>
2021-03-27 13:49:25 -0400

    Reorganized file hierarchy, started chrome extension

--- Comparator.py
+++
@@ -1,9 +0,0 @@
-#Use TFIDF vectorized matrix and run LSA analysis on it
-
-#Use results to seach articles from trusted sites
-
-#Take found articles, extract text and run LSA analysis on them as well
-
-#Compute difference as percentage
-
-
