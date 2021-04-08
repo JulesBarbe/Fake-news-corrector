1b5c908752bbfc6392f5d77c9a9346347b4ea99e
JulesBarbe <ajules32@gmail.com>
2021-04-08 18:07:32 -0400

    deleted

--- Models/lda2vecComparator.py
+++
@@ -1,8 +0,0 @@
-from gensim import models
-import pickle
-
-# unpack tfidf data from preprocessing.py
-with open('tfidf_data', 'rb') as file:
-    data = pickle.load(file)
-
-lda = models.LdaModel(data, num_topics = 15)
